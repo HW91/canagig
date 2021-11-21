79a2271ca3595accb376074325c74d4444f42b3c
HW91 <63923099+HW91@users.noreply.github.com>
2021-11-20 23:19:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function renderListings(features) {
           .addTo(map);
         map.flyTo({
           center: feature.geometry.coordinates,
-          zoom: 6,
+          zoom: 8,
         });
       })
 
